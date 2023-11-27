fd5cc1484d7d306edb7d5b91c50e96853d68b8a7
Thaylla de Santana Bispo dos Santos <100003483+thayllaa@users.noreply.github.com>
2023-11-27 19:22:44 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+int somatorioDivisiveis(int numero) {
+  int somatorio = 0;
+  for (int i = 0; i < numero; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      somatorio += i;
+    }
+  }
+  return somatorio;
+}
+
+void main() {
+  print(somatorioDivisiveis(10));
+  print(somatorioDivisiveis(11));
+}
